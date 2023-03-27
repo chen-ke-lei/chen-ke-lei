5c2b9345c62c66121bad75a767b9b68c300cbc59
chenkelei <chenkelei@qq.com>
2023-03-27 09:56:07 +0800

    FIX

--- src/main/java/com/example/faststart/FaststartApplication.java
+++ src/main/java/com/example/faststart/FaststartApplication.java
@@ -1,9 +1,14 @@
 package com.example.faststart;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.gitee.sunchenbin.mybatis.actable.dao.*","com.example.faststart.*"})
+//扫包路径
+@ComponentScan(basePackages = {"com.gitee.sunchenbin.mybatis.actable.manager.*","com.example.faststart.*"})
 public class FaststartApplication {
 
     public static void main(String[] args) {
